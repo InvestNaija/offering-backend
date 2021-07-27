cd47e57a767a26ab2d2c4d44452ad27a4a922361
InvestNaija <86833132+InvestNaija@users.noreply.github.com>
2021-07-27 12:46:33 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -5,8 +5,7 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "postgres",
-    "native": true
+    "dialect": "postgres"
   },
   "test": {
     "username": "root",
@@ -20,7 +19,6 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "postgres",
-    "native": true
+    "dialect": "postgres"
   }
 }
